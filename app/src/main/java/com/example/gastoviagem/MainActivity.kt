098381d3cdabe285f503e25c66799e4140f0336d
João Pedro <joaobritosts@gmail.com>
2024-04-15 18:46:32 -0300

    Adição da funcionalidade butão

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.gastoviagem
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        observerButtons()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -26,5 +27,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun ButaoCalcular(view: View) {}
+    private fun observerButtons(){
+        binding.buttonCalculate.setOnClickListener {
+            calculate()
+        }
+        binding.buttonBruno.setOnClickListener {
+            Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
+            }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun calculate(){
+        val distance = binding.editDistance.getText().toString().toInt()
+        val price =  binding.editPrice.getText().toString().toInt()
+        val autonomy = binding.editAutonomy.getText().toString().toInt()
+
+        val soma = distance + price + autonomy
+        binding.textTotalValue.text = "R$$soma"
+
+    }
 }
