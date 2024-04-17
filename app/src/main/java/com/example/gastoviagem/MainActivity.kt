1eeada35a43df0b75d2fe81251e4a41578f1dcf9
João Pedro <joaobritosts@gmail.com>
2024-04-17 12:02:54 -0300

    Adiçao da lógica de calculo do App gasto viagem

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.gastoviagem
-
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,31 +18,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        observerButtons()
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.buttonCalculate.setOnClickListener(this)
     }
 
-    private fun observerButtons(){
-        binding.buttonCalculate.setOnClickListener {
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate){
             calculate()
         }
-        binding.buttonBruno.setOnClickListener {
-            Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
-            }
     }
-    @SuppressLint("SetTextI18n")
     private fun calculate(){
-        val distance = binding.editDistance.getText().toString().toInt()
-        val price =  binding.editPrice.getText().toString().toInt()
-        val autonomy = binding.editAutonomy.getText().toString().toInt()
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-        val soma = distance + price + autonomy
-        binding.textTotalValue.text = "R$$soma"
-
+        val totalValue = (distance * price) / autonomy
+        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
     }
 }
